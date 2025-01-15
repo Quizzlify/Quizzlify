ba552db6e646d34d229f7fc4f51a04fc95af68a6
damien <rifflartdamien68@gmail.com>
2025-01-15 21:27:30 +0100

    Page d'accueil et nav bar blur

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
                 </h1>
 
                 <h2 className="mt-3 text-[30px] flex justify-center items-center flex-col">
-                    Tout en s'amusant.
+                    Tout en s&apos;amusant.
                     <svg width="244" height="19" viewBox="0 0 244 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M4 14C26 5.50003 52.7129 1.81453 84.5 5.50007C124 10.0799 121.688 19.5002 161.5 11.5002C201.312 3.50023 225.275 5.50002 240 5.50002"
