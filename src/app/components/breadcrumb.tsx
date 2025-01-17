2491e17e9126cd298b2c34a4c3b39986f6f1c542
AxenoDev <tbavart@gmail.com>
2025-01-17 21:39:32 +0100

    Updated BreadCrump with real size

--- src/app/components/breadcrumb.tsx
+++ src/app/components/breadcrumb.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import { ReactElement } from "react";
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Fragment, ReactElement, useState } from "react";
 
 interface BreadCrumbInterface {
     title: string;
@@ -10,58 +10,83 @@ interface BreadCrumbInterface {
 
 interface BreadcrumbProps {
     isCreateQuiz?: boolean;
-    activeItem: string;
+    level: number;
     onItemChange: (item: string) => void;
 }
 
-const BreadcrumbList: BreadCrumbInterface[] = [
+export const BreadcrumbList: BreadCrumbInterface[] = [
     {
-        title: "Détails",
-        icon: <i className="fa-solid fa-layer-group"></i>
+        title: "Catégorie",
+        icon: <i className="fa-solid fa-layer-group fa-xl"></i>
     },
     {
         title: "Niveau",
-        icon: <i className="fa-solid fa-baby-carriage"></i>
+        icon: <i className="fa-solid fa-baby-carriage fa-xl"></i>
     },
     {
-        title: "Catégories",
-        icon: <i className="fa-solid fa-circle-info"></i>
+        title: "Détails",
+        icon: <i className="fa-solid fa-circle-info fa-xl"></i>
     },
     {
         title: "Questions",
-        icon: <i className="fa-solid fa-circle-question"></i>
+        icon: <i className="fa-solid fa-circle-question fa-xl"></i>
     },
     {
         title: "Résultats",
-        icon: <i className="fa-solid fa-square-poll-horizontal"></i>
+        icon: <i className="fa-solid fa-square-poll-horizontal fa-xl"></i>
     }
 ];
 
 const Breadcrumb: React.FC<BreadcrumbProps> = ({
-                                                   isCreateQuiz = false,
-                                                   activeItem,
-                                                   onItemChange
-                                               }) => {
+    isCreateQuiz = false,
+    level,
+    onItemChange
+}) => {
+    if (level + 1 > BreadcrumbList.length) {
+        throw new Error(`Level ${level} exceeds the number of breadcrumb items.`);
+    }
+
+    const [currentLevel, setCurrentLevel] = useState<number>(level);
+
     const displayedItems = isCreateQuiz
-        ? BreadcrumbList.slice(0, 4) : BreadcrumbList;
+        ? BreadcrumbList.slice(0, 4)
+        : BreadcrumbList;
+
+    const handleSetActiveItem = (item: string) => {
+        setCurrentLevel(displayedItems.findIndex((breadcrumb) => breadcrumb.title === item) + 1);
+        onItemChange(item);
+    }
 
     return (
-        <div className="flex flex-row items-center gap-4">
-            {displayedItems.map((item, index) => (
-                <div key={item.title} className="flex flex-row items-center">
-                    <div
-                        className={`p-2 rounded-lg flex gap-2 items-center cursor-pointer
-                            ${activeItem === item.title ? "bg-accent" : ""}`}
-                        onClick={() => onItemChange(item.title)}
-                    >
-                        {item.icon}
-                        <span>{item.title}</span>
-                    </div>
-                    {index !== displayedItems.length - 1 && (
-                        <i className="fa-solid fa-caret-right ml-4"></i>
-                    )}
-                </div>
-            ))}
+        <div className="flex flex-row items-center gap-2 text-[#333333] font-semibold">
+            {displayedItems.map((item, index) => {
+
+                const isActive = level === index;
+                const isBeforeOrEqualCurrentLevel = index < currentLevel;
+
+                return (
+                    <Fragment key={item.title}>
+                        <div key={item.title} className="flex flex-row items-center text-[20px] font-semibold">
+                            <button
+                                className={`px-[16px] py-[11px] rounded-xl flex gap-[16px] items-center ${isActive
+                                    ? "bg-accent text-white"
+                                    : isBeforeOrEqualCurrentLevel ? "text-accent" : ""}`}
+                                onClick={() => handleSetActiveItem(item.title)}
+                                tabIndex={0}
+                            >
+                                {item.icon}
+                                <span>{item.title}</span>
+                            </button>
+                            {index < displayedItems.length - 1 && (
+                                <span className={`mx-2 ${index < currentLevel - 1 ? "text-accent" : ""}`}>
+                                    <i className="fa-solid fa-caret-right fa-xs"></i>
+                                </span>
+                            )}
+                        </div>
+                    </Fragment>
+                )
+
+            })}
         </div>
     );
 };
